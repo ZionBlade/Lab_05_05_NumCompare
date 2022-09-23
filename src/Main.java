577f4ba59e58d6e17a4c619c86fe24375a057989
ZionBlade <wernerct@mail.uc.edu>
2022-09-22 22:12:12 -0400

    finished commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,32 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int numOne = 15;
+        int numTwo = 19;
+
+        System.out.println("Please enter the first number you would like to compare: ");
+
+        System.out.println("Please enter the second number you would like to compare: ");
+
+        if (numOne == numTwo)
+        {
+            System.out.println("Both numbers are equal.");
+        }
+        else if (numOne >= numTwo)
+        {
+            System.out.println("Both numbers are NOT equal. " + numTwo + " is less than " + numOne);
+        }
+        else if (numOne <= numTwo)
+        {
+            System.out.println("Both numbers are NOT equal. " + numOne + " is less than " +numTwo);
+        }
+        else
+        {
+            System.out.println("That is not a valid number");
+        }
+
+
+
+
     }
 }
